7b10c0cd25fe846d4c9cd7b89ccfee1a25bd0ec1
unknown <masamata98@gmail.com>
2022-04-26 12:46:40 -0700

    some editing in the javascript file to switch This is an edit in javascript to allow swap

--- app/Js/script.js
+++ app/Js/script.js
@@ -4,11 +4,10 @@ const feed=document.querySelector("#feed")
 const rating= document.querySelector("#rating") 
 const button=document.querySelector("button")
 
-console.log(rating)
-
-rate.forEach((number)=>{
+rate.forEach((number,index)=>{
   number.addEventListener('click',()=>{
-    if(number.textContent){
+    if(number.textContent==index+1){
+      number.classList.toggle("clicked")
    button.addEventListener("click",()=>{
       input.textContent=number.textContent;  
       rating.classList.add("hidden")
